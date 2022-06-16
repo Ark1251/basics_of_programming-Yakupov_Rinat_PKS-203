8db98f5f03600a918f5b1a0dd58c54b29a9c3b6f
Ark1251 <102961576+Ark1251@users.noreply.github.com>
2022-06-17 00:01:50 +0300

    Lab 1.2-1.3

---
+++ Lab 1.2-1.3/nForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab_1._2
+{
+    public partial class nForm : Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
